586ad6a1be4076af3db04f7b1b8c770358299edd
Brad Traversy <traversymedia@gmail.com>
2020-04-14 12:54:27 -0400

    Added header component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+import Header from './components/Header';
 import './scss/app.scss';
 
-const app = async () => {};
+const app = async () => {
+	document.getElementById('header').innerHTML = Header();
+};
 
 // Load app
 app();
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+	const template = `
+    <header>
+      <h1>My Parcel App</h1>
+      <p>This is a boilerplate for a simple vanilla JS workflow using the Parcel bundler</p>
+    </header>
+  `;
+
+	return template;
+};
+
+export default Header;
